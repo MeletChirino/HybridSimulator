2d55de1dbfc85799be94d09b09b782c63a1fb9b1
Melet David <melet.chirino@gmail.com>
2022-04-05 23:24:46 +0200

    [FIX] sintax bugs

--- classes.py
+++ classes.py
@@ -1,10 +1,11 @@
-class InPort(Port):
+class InPort:
     def __init__(self, name):
         self.name = name
         self.value = 0
         self.type = "In"
     def update(self, value):
         self.value = value
+        print(F"{self.type}-{self.name}: updated to => {self.value}")
 
 class OutPort:
     def __init__(self, name):
@@ -23,6 +24,7 @@ class OutPort:
         self.value = value
         for port in self.connections:
             port.update(self.value)
+        print(F"{self.type}-{self.name}: updated to => {self.value}")
 
 class Component:
     def __init__(self, name, in_ports, out_ports, **kwargs):
@@ -35,15 +37,19 @@ class Component:
         self.actual_state = 0
         self.te = 0.0
         self.tr = 0.0
+
     def delta_int(self, state):
-        # returns next state if timeout event
-        pass
+    # returns next state if timeout event
+        return 0
+
     def delta_ext(self, state, event):
-        # returns next state if discret event
-        pass
+    # returns next state if discret event
+        return 0
+
     def ta(self, state):
-        # returns timeout of certain state
-        pass
-    def lambda(self, state, output):
-        # return new output value when timeout 
-        pass
+    # returns timeout of certain state
+        return 0
+
+    def lambdia(self, state, output):
+    # return new output value when timeout 
+        return 0
