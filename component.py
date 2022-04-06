ff01c4db16f89e4efff36e9c77b1f6675d31d98d
Juan Beroy <juan.beroy1@gmail.com>
2022-04-06 09:08:24 +0200

    Updated components

--- component.py
+++ component.py
@@ -1,3 +1,24 @@
-class Proc:
+from classes import Component
+import math
+
+class Proc(Component):
     def __init__(self, name, **kwargs):
-        self.name = name
+        Component.__init__.super(self, name)
+
+def internal(self):
+    if self.current_state == 1:
+        self.current_state == 0
+
+def external(self, req):
+    if req == 1:
+        self.current_state == 1
+
+def avancement(self):
+    if self.current_state == 0:
+        return math.inf
+    elif self.current_state == 1:
+        return 3.0
+
+def generate_output(self):
+    if self.current_state == 1:
+        done = True
