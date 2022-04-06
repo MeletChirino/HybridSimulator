9f5b7f66beb13af728b491076d5948bef340beec
Juan Beroy <juan.beroy1@gmail.com>
2022-04-06 10:41:19 +0200

    Modifed Buffer in Components

--- component.py
+++ component.py
@@ -1,29 +1,4 @@
 from classes import Component
-<<<<<<< HEAD
-import math
-
-class Proc(Component):
-    def __init__(self, name, **kwargs):
-        Component.__init__.super(self, name)
-
-def internal(self):
-    if self.current_state == 1:
-        self.current_state == 0
-
-def external(self, req):
-    if req == 1:
-        self.current_state == 1
-
-def avancement(self):
-    if self.current_state == 0:
-        return math.inf
-    elif self.current_state == 1:
-        return 3.0
-
-def generate_output(self):
-    if self.current_state == 1:
-        done = True
-=======
 from tools import *
 from math import inf
 
@@ -35,8 +10,8 @@ class Proc(Component):
         if(self.current_state == 1):
             self.current_state = 0
 
-    def external(self):
-        if(self.current_state and exist(self.event_list.exist()):
+    def external(self, port):
+        if(self.current_state == 0):
             self.current_state = 1
 
     def avance(self):
@@ -47,12 +22,17 @@ class Proc(Component):
             return 3.0
         return -1
     def generate_output(self):
-        if self.current_state == 1):
+        if (self.current_state == 1):
             self.output[0].update_value(1)
+            self.tr = self.avance()
+    
+    def conflict(self):
+        pass
 
 class Generator(Component):
     def init(self):
         self.current_state = 0
+        self.tr = 2.0
     def avance(self):
         return 2.0
     def internal(self):
@@ -61,5 +41,53 @@ class Generator(Component):
         pass
     def generate_output(self):
         self.output[0].update_value = 1
+        self.tr = self.avance()
+    
+    def conflict(self):
+        pass
 
->>>>>>> 2e1ca9ff4b4875b7022af311e2bf842ecbd9ab72
+class Buffer(Component):
+    def init(self):
+        self.current_state = 0
+        self.q = 0
+
+    def internal(self):
+        if(self.current_state == 1):
+            self.current_state = 2
+            self.q -= 1
+
+    def external(self, port):
+        import pdb; pdb.set_trace()
+        if(self.current_state == 0 and port == self.input[0]):
+            self.current_state = 1
+            self.q += 1
+
+        if(self.current_state == 1 and port == self.input[0]):
+            self.q += 1
+        
+        if(self.current_state == 2 and port == self.input[0]):
+            self.q += 1
+        
+        if(self.current_state == 2 and self.q > 0 and port == self.input[1]):
+            self.current_state = 1
+
+        if(self.current_state == 2 and self.q == 0 and port == self.input[1]):
+            self.current_state = 0
+    
+    def avance(self):
+        if self.current_state == 0:
+            return inf
+        elif self.current_state == 1:
+            return 0
+        if self.current_state == 2:
+            return inf
+        return -1
+    
+    def generate_output(self):
+        if (self.current_state == 1):
+            self.output[0].update_value(1)
+        self.tr = self.avance()
+    
+    def conflict(self):
+        self.external
+
